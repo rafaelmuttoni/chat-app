06a7914a611a68f879bf7198216091f0212dcb25
Rafael Muttoni <hi@rafaelmuttoni.com>
2020-10-02 07:27:35 -0300

    SignIn component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore"; // database
+import "firebase/auth"; // user auth
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+import ChatRoom from "./components/ChatRoom";
+import SignIn from "./components/SignIn";
+import SignOut from "./components/SignOut";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBVto-9wL57wiTsCauCyJPhB4eaLKQWKjM",
+  authDomain: "chat-app-f2ad9.firebaseapp.com",
+  databaseURL: "https://chat-app-f2ad9.firebaseio.com",
+  projectId: "chat-app-f2ad9",
+  storageBucket: "chat-app-f2ad9.appspot.com",
+  messagingSenderId: "622803625168",
+  appId: "1:622803625168:web:065208df3af23f5899ead5",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn firebase={firebase} auth={auth} />}
+        {auth.currentUser && <SignOut auth={auth} />}
+      </section>
     </div>
   );
 }
